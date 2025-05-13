d4eab596dd4ec8e36f93f2d2714de2726335d715
Uzonna Alexander <uzonna101@outlook.com>
2025-05-13 18:03:43 -0400

    Updated Header Buttons
    Header buttons now obviously display which page you're on

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,27 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+
 
 const Header = () => {
+    const router = useRouter();
+    const currentPath = router.pathname;
+
+    const defaultColor = "text-yellow-100 hover:text-white";
+    const activeColor = "text-yellow-100 font-bold border-b-2 border-yellow-300 hover:text-white hover:border-white";
     return (
         <section>
             <div className="flex justify-center bg-[#125561] text-7xl py-4 pt-12 [font-family:'Cinzel',serif] font-medium text-white">Alex Alexander</div>
             <div className="bg-black py-1 flex justify-center gap-20">
-                <Link href="/" className="text-yellow-100 hover:text-white">
+                <Link href="/" className={currentPath === '/' ? activeColor : defaultColor}>
                     HOME
                 </Link>
 
-                <Link href="/books" className="text-yellow-100 hover:text-white">
+                <Link href="/books" className={currentPath === '/books' ? activeColor : defaultColor}>
                     BOOKS
                 </Link>
 
-                <Link href="/bio" className="text-yellow-100 hover:text-white">
+                <Link href="/bio" className={currentPath === '/bio' ? activeColor : defaultColor}>
                     BIO
                 </Link>
             </div>
