708f579812aa01247a751b518a1938825a9fa3a1
Uzonna Alexander <uzonna101@outlook.com>
2025-05-16 12:42:31 -0400

    Solo Book Pages Complete - Site in Beta
    NEW: Each book has its own page with completed formatting (PC and Mobile)
    
    With this, the site has all basic features and formatting and only needs text/images to be complete

--- src/pages/books/lost-keys.js
+++ src/pages/books/lost-keys.js
@@ -1,10 +1,49 @@
 export default function LostKeys() {
   return (
-    <section className="min-h-screen bg-white text-black p-8">
-      <h1 className="text-4xl font-bold mb-4">The Lost Keys of Mechanism</h1>
-      <p className="text-lg">
-        Alex Alexander is....
-      </p>
+    <section className="px-6 py-10 max-w-6xl mx-auto">
+      
+      <h2 className="text-3xl font-bold mb-3 text-center md:text-left">The Lost Keys of Mechanism</h2>
+
+      <img
+        src="/images/lost-keys_cover.jpg"
+        alt="Lost Keys"
+        className="float-none md:float-left w-full md:w-1/2 max-w-sm mb-4 border shadow-md md:mr-5"
+      />
+
+      <div className="mb-4">
+        <p><span className="font-bold">Published by: </span>Independently Published</p>
+        <p><span className="font-bold">Release Date: </span>December 2, 2022</p>
+        <p><span className="font-bold">Buy the Book: </span> <span><a href="https://www.amazon.ca/Lost-Keys-Mechanism-Alexis-Alexander/dp/B0BNTXPR3Z" 
+            className='text-blue-400  hover:border-b-1 hover:border-blue-400'
+            target="_blank" rel="noopener noreferrer">Amazon</a></span></p>
+      </div>
+
+      {/* <div className="mb-4">
+        <p className="font-bold">Add Text Here</p>
+      </div> */}
+
+      <div className="space-y-2">
+        <p>Rituals are performed as one progresses through the degrees of Mechanism in the form of initiation. Yet even many initiated Mechanics themselves often find it difficult to understand the esoteric aspects within the lore of Mechanism.</p>
+        <p>This book, titled <span className="font-bold">The Lost Keys of Mechanism</span>, is written for initiated Mechanics. It has been prepared in the spirit of brotherhood — in the spirit of friendship, truth, and love — with the purpose of shedding light on the esoteric or hidden teachings found within some of the rituals of Mechanism.</p>
+        <p>An initiated Mechanic should come to see and understand that symbols can represent many ideas or concepts. They have the power to both conceal and reveal truth, to remind us of lessons once learned, and to instruct us on multiple levels of understanding. Through these teachings, one gains greater knowledge, understanding, and wisdom as one journeys through life.</p>
+      </div>
+
+      {/* <div className="mb-4">
+        <p>Add Text Here</p>
+      </div> */}
+      <div className="clear-both"/>
+
+      <div className="mb-4 mt-10">
+        <p className="font-bold">Praise</p>
+      </div>
+
+      <div className="mb-4">
+        <p>"A unique read that encourages individuals to seek answers in a more grounded yet practical way. With various symbols that have since lost their meaning and significance, the author does a great job of bringing light to their different essences concisely yet pragmatically."</p>
+        <p className="font-bold">—The Moving Words</p>
+      </div>
+      
+
+      
     </section>
   )
 }
--- src/pages/books/seven-arts.js
+++ src/pages/books/seven-arts.js
@@ -1,10 +1,57 @@
 export default function SevenArts() {
   return (
-    <section className="min-h-screen bg-white text-black p-8">
-      <h1 className="text-4xl font-bold mb-4">An Initiated Mechanic&apos;s View of The Seven Liberal Arts and Sciences</h1>
-      <p className="text-lg">
-        Alex Alexander is....
-      </p>
+    <section className="px-6 py-10 max-w-6xl mx-auto">
+      
+      <h2 className="text-3xl font-bold mb-3 text-center md:text-left">An Initiated Mechanic&apos;s View of The Seven Liberal Arts and Sciences</h2>
+
+      <img
+        src="/images/seven-arts_cover.jpg"
+        alt="Lost Keys"
+        className="float-none md:float-left w-full md:w-1/2 max-w-sm mb-4 border shadow-md md:mr-5"
+      />
+
+      <div className="mb-4">
+        <p><span className="font-bold">Published by: </span>Author House</p>
+        <p><span className="font-bold">Release Date: </span>October 17, 2022</p>
+        <p><span className="font-bold">Buy the Book: </span> <span><a href="https://www.amazon.ca/Initiated-Mechanics-Seven-Liberal-Sciences/dp/1665571918" 
+            className='text-blue-400  hover:border-b-1 hover:border-blue-400'
+            target="_blank" rel="noopener noreferrer">Amazon</a></span></p>
+      </div>
+
+      {/* <div className="mb-4">
+        <p className="font-bold">Add Text Here</p>
+      </div> */}
+
+      <div className="space-y-2">
+        <p><span className="font-bold">An Initiated Mechanic&apos;s View of the Seven Liberal Arts and Sciences</span> is written specifically for initiated Mechanics. It has been prepared in the spirit of brotherhood with the purpose of shedding light on the Liberal Arts.</p>
+        <p>In the Second Degree of Mechanism, which is the first of four degrees belonging to the Fellow Craft Mechanics, the ritual states:</p>
+        <p className="font-bold">“The study of the liberal arts, that valuable branch of education, which tends so effectually to polish and adorn the mind, is earnestly recommended to your consideration, especially the science of geometry or Mechanism.”</p>
+        <p>However, the ritual does not explain what the liberal arts are, nor the order in which they are structured.</p>
+        <p>The Seven Liberal Arts and Sciences, as they are traditionally known, served as the foundation of education in antiquity and throughout the Middle Ages. At that time, they were considered the complete body of knowledge necessary for a well-rounded education. Referred to in Latin as <span className="italic">artes liberales</span>, from <span className="italic">liber</span> meaning "free," they were the subjects available to free men. These stood in contrast to the <span className="italic">artes illiberales</span>, which were practical or vocational skills taught for the purpose of earning a living. These were the operative arts of laborers and were considered less prestigious as educational pursuits.</p>
+        <p>Although the liberal arts were formally adopted during the Medieval period, their roots extend much further back to the Pythagorean and Platonic eras. They were structured to reflect a progressive path in education. The first three subjects are Grammar, Logic, and Rhetoric. Together, they are known as the Trivium, a term that means “the path of three roads.” These subjects were regarded as the foundational intellectual tools that lead to the truth of the mind.</p>
+        <p>The remaining four subjects are Arithmetic, Geometry, Music, and Astronomy. These form what is called the Quadrivium, a name derived from the Latin word quatuor, meaning “four.” These four disciplines were considered the path to understanding the truth of matter.</p>
+        <p>When one studies the subjects that comprise the Trivium and the Quadrivium, specific regions of the brain are stimulated in ways that enhance cognitive function and natural mental ability. This results in the transformation of mental processes from disorder to order, allowing one to pursue and accomplish any goal in life more effectively.</p>
+        <p>This is why initiated Mechanics are strongly encouraged to study the Liberal Arts. These disciplines truly "polish and adorn the mind," increasing awareness and responsiveness to one’s surroundings, which are essential qualities for the journey of the initiated Mechanic.</p>
+
+      </div>
+
+      {/* <div className="mb-4">
+        <p>Add Text Here</p>
+      </div> */}
+
+      <div className="clear-both"/>
+
+      <div className="mb-4 mt-10">
+        <p className="font-bold">Praise</p>
+      </div>
+
+      <div className="mb-4">
+        <p>"Add Text Here"</p>
+        <p className="font-bold">—Unknown</p>
+      </div>
+      
+
+      
     </section>
   )
 }
